26952fef66f56d02a697defb026b95e9a51c2b11
BuizelKing <judsonwjones@gmail.com>
2020-03-25 12:39:28 -0500

    Revert "Merge branch 'Noah-Chad' into BuizelKing"
    This reverts commit 1576b0ef8f80c8d6524c71b3905b7349a8664e59, reversing
    changes made to 77c3bf5797b18902f327da61308e699b7d2abf1d.

--- CelestialPioneers/Intermediate/Build/Win64/CelestialPioneersEditor/Development/Engine/SharedPCH.Engine.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/229133/Desktop/GitHub/CelestialPioneer/CelestialPioneers/Intermediate/Build/Win64/CelestialPioneersEditor/Development/Engine/SharedPCH.Engine.h"
--- CelestialPioneers/Source/CelestialPioneers/CelestialPioneers.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "CelestialPioneers.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CelestialPioneers, "CelestialPioneers" );
